75c994fa36f14f358f1de7f60a96f57a355bc2b9
zhiyongli <zhiyongli@microsoft.com>
2022-07-08 17:22:34 +0800

    Pod Intent

--- examples/asa/namespaced/setup.sh
+++ examples/asa/namespaced/setup.sh
@@ -10,9 +10,9 @@ kubectl create secret docker-registry private-registry-credentials --docker-serv
 echo 'Create secret to read the cluster resource'
 kubectl create secret docker-registry shared-registry-credentials --docker-server=serviceacr.azurecr.io --docker-username=serviceacr --docker-password=1dXPkDXTRv+DlZxi/n8HRC0IjVdKRadY --docker-email=zhiyongli@microsoft.com
 
-read -p "Input the subscription id for container app: " subscriptionid
-read -p "Input the resourcegroup for container app: " resourcegroup
-read -p "Input the ask name for container app: " aksname
+read -e -p "Input the subscription id for container app: " -i "d51e3ffe-6b84-49cd-b426-0dc4ec660356" subscriptionid
+read -e -p "Input the resourcegroup for container app: " -i "zhiyongli" resourcegroup
+read -e -p "Input the ask name for container app: " -i "containerapp" aksname
 az account set --subscription $subscriptionid
 az aks get-credentials --resource-group $resourcegroup --name $aksname --admin --overwrite-existing --file ./containerapp.yaml
 
